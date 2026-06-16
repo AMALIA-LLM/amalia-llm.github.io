f57efdfc83e1fde8224eddb14273dd9f477db30c
Rui Guerra <rp.guerra@fct.unl.pt>
2026-06-16 16:34:05 +0100

    branding and links

--- source/conf.py
+++ source/conf.py
@@ -27,3 +27,30 @@ exclude_patterns = []
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
 html_title = 'Documentação AMALIA'
+html_theme_options = {
+   "logo": {
+      "image_light": "_static/logo/logo-color-black.png",
+      "image_dark": "_static/logo/logo-color-white.png",
+   },
+   "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/AMALIA-LLM",
+            "icon": "fa-brands fa-square-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "HuggingFace",
+            "url": "https://huggingface.co/carminho",
+            "icon": "fa-brands fa-hugging-face",
+            "type": "fontawesome"
+        }
+    ]
+}
+html_favicon = '_static/favicon.png'
+html_js_files = [
+    (
+        "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@7.2.0/js/brands.min.js",
+        {"defer": "defer"},
+    ),
+]
